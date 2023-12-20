e4d2d2375c7621c2b9832a55aaef3e680e6ea40f
John Owens <john.owens@opencastsoftware.com>
2023-12-20 09:47:41 +0000

    Added resdis module

--- RedisDemo/src/main/java/Main.java
+++ RedisDemo/src/main/java/Main.java
@@ -1,7 +1,23 @@
+import redis.clients.jedis.Jedis;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        testRedis();
+    }
+
+    public static void testRedis() {
+        Jedis jedis = new Jedis("localhost", 6379);
+
+        String status = jedis.set("Bike:1", "Rosebud");
+
+        if (status.contains("OK")) {
+            System.out.println("Save data");
+        } else {
+            System.out.println("Didn't save data");
+        }
     }
 
 }
